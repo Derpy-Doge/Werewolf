a21a28a3e5c8a89a5d5234349f8415e9215e804b
MUKILTEO\1549673 <1549673@mukilteo.wednet.edu>
2025-11-20 13:25:21 -0800

    started AI and finished most UI if not all

--- Assets/Enemy_AI.cs
+++ Assets/Enemy_AI.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Enemy_AI : MonoBehaviour
 {
+    public Rigidbody myRigidbody;
+    public GameObject player;
+    public float movespeed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,10 @@ public class Enemy_AI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.transform.position.x < 0)
+        {
+            Vector3 newVelocity = myRigidbody.linearVelocity;
+            newVelocity.z = -movespeed;
+        }
     }
 }
