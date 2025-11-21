fe77602fa043a6d724209f7fa78ae07ce4703e04
MUKILTEO\1549673 <1549673@mukilteo.wednet.edu>
2025-11-21 13:25:00 -0800

    Enemy AI almost finished

--- Assets/Enemy_AI.cs
+++ Assets/Enemy_AI.cs
@@ -5,6 +5,8 @@ public class Enemy_AI : MonoBehaviour
     public Rigidbody myRigidbody;
     public GameObject player;
     public float movespeed = 10f;
+    public float xfollow;
+    public float zfollow;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,10 +16,18 @@ public class Enemy_AI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player.transform.position.x < 0)
+        if (Mathf.Sqrt((player.transform.position.x - transform.position.x) * (player.transform.position.x - transform.position.x) + (player.transform.position.z - transform.position.z) * (player.transform.position.z - transform.position.z)) <= 5)
         {
-            Vector3 newVelocity = myRigidbody.linearVelocity;
-            newVelocity.z = -movespeed;
+            Vector3 newVelocity = new (xfollow, 0, zfollow);
+            newVelocity.x = movespeed;
+            newVelocity.z = movespeed;
+            myRigidbody.linearVelocity = newVelocity;
         }
+        
+    }
+    void FixedUpdate()
+    {
+        xfollow = (player.transform.position.x - transform.position.x);
+        zfollow = (player.transform.position.z - transform.position.z);
     }
 }
